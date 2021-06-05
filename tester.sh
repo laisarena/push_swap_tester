e720cd5849bbe358eedf6b3cf017ab32422dd98d
Lais Arena <laisarena@gmail.com>
2021-06-05 12:47:43 -0300

    Include error handling in case the checker is not working

--- tester.sh
+++ tester.sh
@@ -1,29 +1,42 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    test.sh                                            :+:      :+:    :+:    #
+#    tester.sh                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: lfrasson <lfrasson@student.42sp.org.br     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/04 16:53:09 by lfrasson          #+#    #+#              #
-#    Updated: 2021/06/04 16:53:38 by lfrasson         ###   ########.fr        #
+#    Updated: 2021/06/05 12:34:47 by lfrasson         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
+NUM_TESTS=100
+PUSH_SWAP=./push_swap
+CHECKER=./checker
+CKER=1
+RET_CKER="is not working"
+
 GREEN="\033[32m"
 YELLOW="\033[0;33m"
 RED="\033[0;31m"
+BLUE="\033[0;34m"
 MUTED="\033[1;30m"
 RESET="\033[0m"
 BOLD="\033[1m"
+
+if ! command -v $CHECKER &> /dev/null
+then
+	echo -en "${RED}${BOLD}\n./checker is not working.\nSo this script will only check the number of operations\n${RESET}"
+	CKER=0
+fi
 
 error()
 {
 	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
 	ARG="$2"
-	RET=$(./push_swap $ARG 2>&1)
+	RET=`$PUSH_SWAP $ARG 2>&1`
 	if [ "$RET" != "Error" ];
 		then
 		echo -e "${RED}Fail${RESET}"
@@ -36,7 +49,7 @@ do_noting()
 {
 	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
 	ARG="$2"
-	RET=$(./push_swap $ARG 2>&1)
+	RET=`$PUSH_SWAP $ARG 2>&1`
 	if [ "$RET" != "" ];
 		then
 		echo -e "${RED}Fail${RESET}"
@@ -49,13 +62,16 @@ checker3()
 {
 	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
 	ARG="$2"
-	RET1=`./push_swap $ARG | ./checker $ARG`
-	RET2=`./push_swap $ARG | wc -l | bc`
-	if [ "$RET1" == "OK" ] && (($RET2 == 2)) || (($RET2 == 3));
+	if (( $CKER == 1))
+	then
+		RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+	fi
+	RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+	if [ "$RET_CKER" == "OK" ] && (($RET2 == 2)) || (($RET2 == 3));
 		then
-			echo -e "${GREEN}Success (checker $RET1 | $RET2 instructions)${RESET}"
+			echo -e "${GREEN}Success (checker $RET_CKER | $RET2 instructions)${RESET}"
 	else
-		echo -e "${RED}Fail (checker $RET1 | $RET2 instructions)${RESET}"
+		echo -e "${RED}Fail (checker $RET_CKER | $RET2 instructions)${RESET}"
 	fi
 }
 
@@ -63,13 +79,16 @@ checker5()
 {
 	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
 	ARG="$2"
-	RET1=`./push_swap $ARG | ./checker $ARG`
-	RET2=`./push_swap $ARG | wc -l | bc`
-	if [ "$RET1" == "OK" ] && (($RET2 <= 12));
+	if (( $CKER == 1))
+	then
+		RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+	fi
+	RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+	if [ "$RET_CKER" == "OK" ] && (($RET2 <= 12));
 		then
-			echo -e "${GREEN}Success (checker $RET1 | $RET2 instructions)${RESET}"
+			echo -e "${GREEN}Success (checker $RET_CKER | $RET2 instructions)${RESET}"
 	else
-		echo -e "${RED}Fail (checker $RET1 | $RET2 instructions)${RESET}"
+		echo -e "${RED}Fail (checker $RET_CKER | $RET2 instructions)${RESET}"
 	fi
 }
 
@@ -79,12 +98,15 @@ random_checker()
 	ERR=0
 	sum=0
 	count=0
-	for i in range {1..9}
+	for ((i = 0; i < NUM_TESTS; i++))
 		do 
 			ARG=`ruby -e "puts $2.to_a.shuffle.join(' ')"`
-			RET1=`./push_swap $ARG | ./checker $ARG`
-			RET2=`./push_swap $ARG | wc -l | bc`
-			if [ "$RET1" != "OK" ];
+			if (( $CKER == 1))
+			then
+				RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+			fi
+			RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+			if [ "$RET_CKER" != "OK" ];
 				then
 				((ERR++))
 				echo -en "${RED}▓$RET2▓${RESET}"
@@ -111,7 +133,7 @@ echo -e "\n${BOLD}Error management${RESET}\n"
 error "Non numeric parameters" "3 aa 1"
 error "Duplicate numeric parameter" "3 3 1"
 error "Only numeric parameters including one greater than MAX INT" "3 1 2147483648"
-error "Only numeric parameters including one greater than MIN INT" "3 1 -2147483649"
+error "Only numeric parameters including one less than MIN INT" "3 1 -2147483649"
 
 echo -e "\n${BOLD}Identity test${RESET}\n"
 do_noting "Without any parameters"
@@ -127,8 +149,6 @@ random_checker "Random list of Five numbers" "(0..4)" 12
 echo -e "\n${BOLD}Middle version${RESET}\n"
 echo -e "less than 700\t-> 5\nless than 900\t-> 4\nless than 1100\t-> 3\nless than 1300\t-> 2\nless than 1500\t-> 1\n"
 random_checker "Random list of hundred numbers -50 to 49" "(-50..49)" 1500
-random_checker "Random list of hundred numbers -99 to 0" "(-99..0)" 1500
-random_checker "Random list of hundred numbers 0 to 99" "(0..99)" 1500
 
 echo -e "\n${BOLD}Advanced version${RESET}\n"
 echo -e "less than 5500\t-> 5\nless than 7000\t-> 4\nless than 8500\t-> 3\nless than 10000\t-> 2\nless than 11500\t-> 1\n"
